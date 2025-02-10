5f9c2c92eb3820d7bd8558c32289d951b62882e0
Uzrakhan <uzrakhan539@gmail.com>
2025-02-10 12:20:35 +0530

    Completed Product details component

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
 import products from '../products';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
+import {faLocationCrosshairs} from '@fortawesome/free-solid-svg-icons';
 
 const ProductDetails = () => {
     const { id } = useParams();
-
     const product = products.find((item) => item.id === parseInt(id));
+
+    const locations = ['Mumbai', 'Delhi', 'Noida','Kolkata','Varanasi', 'Chennai','Bhopal','Indore'];
+    const [selectedLocation,setSelectedLocation] = useState('');
 
     if(!product) {
         return <p>No product found.</p>
@@ -13,11 +18,34 @@ const ProductDetails = () => {
 
     return(
         <div className='product-det-card'>
-            <div className=''>
+            <div className='product-det-img'>
                 <img src={product.image} alt='product.name' width="100px" height='100px'/>
             </div>
-            <h3>{product.name}</h3>
-            <span>{product.price}</span>
+            <div className='product-det-info'>
+                <p style={{color: 'gray'}}>{product.category}</p>
+                <h3>{product.name}</h3>
+                <div>
+                    {product.ratings}   
+                    <FontAwesomeIcon icon={faStar} />
+                </div>
+                <span style={{fontWeight: 'bold', marginTop: '1vw', fontSize: '2vw'}}>₹{product.price}</span>
+                <div className='location-det'>
+                    Deliver to:
+                    <select value={selectedLocation} onChange={(e) => setSelectedLocation(e.target.value)}>
+                        <option value='' disabled>
+                            Select a location
+                        </option>
+                        {locations.map((location,index) => (
+                            <option key={index} value={location}>{location}</option>
+                        ))}
+                    </select>
+
+                    {selectedLocation && (
+                        <p>Delivery will be made to: <strong>{selectedLocation}</strong></p>
+                    )}
+                </div>
+                <p style={{color: 'gray', marginTop: '2vw'}}>Description: {product.description}</p>
+            </div>
         </div>
     )
 };
--- src/products.jsx
+++ src/products.jsx
@@ -4,140 +4,167 @@ const products = [
       name: 'Men Printed Round Neck Pure Cotton Black T-Shirt',
       price: 255,
       category: 'Fashion(Men)',
-      image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/0/y/g/xl-anime-tshirt-for-men-zen1fashion-original-imah3dwrzjgjbyba.jpeg?q=70'
+      image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/0/y/g/xl-anime-tshirt-for-men-zen1fashion-original-imah3dwrzjgjbyba.jpeg?q=70',
+      description: 'Stay stylish and comfortable with this printed black round neck T-shirt made from pure cotton. Perfect for casual outings or a relaxed day.',
+      ratings: 4.5
     },
     {
       id: 2,
       name: 'Men Colorblock Round Neck Pure Cotton Green, Black T-Shirt',
       price: 229,
       category: 'Fashion(Men)',
-      image: 'https://rukminim2.flixcart.com/image/612/612/kfoapow0-0/t-shirt/w/l/7/s-daredevil-party-wear-wrath-original-imafw2kc8ducmcbp.jpeg?q=70'
+      image: 'https://rukminim2.flixcart.com/image/612/612/kfoapow0-0/t-shirt/w/l/7/s-daredevil-party-wear-wrath-original-imafw2kc8ducmcbp.jpeg?q=70',
+      description: 'Add a pop of color to your wardrobe with this colorblock round neck T-shirt. Made from pure cotton for all-day comfort.',
+      ratings: 3.5
     },
     {
       id: 3,
       name: 'Men Self Design Polo Neck Polyester Brown T-Shirt',
       price: 299,
       category: 'Fashion(Men)',
-      image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/2/d/l/l-polo-8014-kajaru-original-imah75gnqkcp9ytd.jpeg?q=70'
+      image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/2/d/l/l-polo-8014-kajaru-original-imah75gnqkcp9ytd.jpeg?q=70',
+      description: 'This self-designed brown polo neck T-shirt made of polyester is a great choice for a sleek and trendy look.',
+      ratings: 2
     }, 
     {
       id: 4,
       name: 'Men Printed Hooded Neck Cotton Blend Black, Purple T-Shirt',
       price: 279,
       category: 'Fashion(Men)',
-      image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/y/t/g/m-jc23-hd-fs-black-purple-chicago-jump-cuts-original-imagtbghtxauftfs.jpeg?q=70'
+      image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/y/t/g/m-jc23-hd-fs-black-purple-chicago-jump-cuts-original-imagtbghtxauftfs.jpeg?q=70',
+      description: 'Step out in style with this hooded T-shirt featuring a vibrant black and purple print. Crafted from a soft cotton blend.',
+      ratings: 4.5
     },
     {
       id: 5,
       name: 'Men Solid Polo Neck Polyester Green T-Shirt',
       price: 249,
       category: 'Fashion(Men)',
-      image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/u/s/8/s-ts36-vebnor-original-imah8syvubganrmt.jpeg?q=70'
+      image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/u/s/8/s-ts36-vebnor-original-imah8syvubganrmt.jpeg?q=70',
+      description: 'Elevate your casual wear with this solid green polo neck T-shirt. Made from durable polyester for everyday wear.',
+      ratings: 4.54
     }, 
     {
         id: 6,
         name: 'Men Solid Round Neck Polyester Pink T-Shirt',
         price: 279,
         category: 'Fashion(Men)',
-        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/g/e/m/m-fmt241-pogo-pk-force-original-imah5fs5zzphdwst.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/g/e/m/m-fmt241-pogo-pk-force-original-imah5fs5zzphdwst.jpeg?q=70',
+        description: 'Brighten your outfit with this solid pink round neck T-shirt made of lightweight polyester fabric for easy mobility.',
+        ratings: 4.94
     },
     {
         id: 7,
         name: 'Men Embroidered Cotton Blend Straight Kurta  (Purple)',
         price: 679,
         category: 'Fashion(Men)',
-        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/g/c/g/xl-mirror-kurta-tezys-original-imah6etpyptyqrzc.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/g/c/g/xl-mirror-kurta-tezys-original-imah6etpyptyqrzc.jpeg?q=70',
+        description: 'This purple embroidered straight kurta is perfect for festive and formal occasions. Made from a comfortable cotton blend.',
+        ratings: 4.5
     },
     {
         id: 8,
         name: 'Men Solid Cotton Blend Straight Kurta  (Black)',
         price: 245,
         category: 'Fashion(Men)',
-        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/p/3/j/m-plain-kurta-triveni-creation-original-imahybf9mf6nftmu.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/p/3/j/m-plain-kurta-triveni-creation-original-imahybf9mf6nftmu.jpeg?q=70',
+        description: 'Keep it classy with this solid black straight kurta. Crafted from a breathable cotton blend for a relaxed fit.'
     },
     {
         id: 9,
         name: 'Men Printed Cotton Blend Straight Kurta  (White)',
         price: 395,
         category: 'Fashion(Men)',
-        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/1/i/a/xl-derfv04-deelmo-original-imah72ypppfuw36x.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/1/i/a/xl-derfv04-deelmo-original-imah72ypppfuw36x.jpeg?q=70',
+        description: 'Make a statement with this white printed kurta. Designed from a cotton blend, it’s ideal for both casual and formal events.'
     }, 
     {
         id: 10,
         name: 'Men Solid Cotton Blend Straight Kurta  (Light Blue)',
         price: 399,
         category: 'Fashion(Men)',
-        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/v/p/m/m-kurta1501-zartha-original-imagsuzdfeyy2wgh.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/v/p/m/m-kurta1501-zartha-original-imagsuzdfeyy2wgh.jpeg?q=70',
+        description: 'This light blue solid kurta offers a sophisticated look. Made from a soft cotton blend for comfort throughout the day.'
     },
     {
         id: 11,
         name: 'realme P1 5G (Feather Blue, 128 GB)  (6 GB RAM)',
         price: 14999,
         category: 'Electronics',
-        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/y/x/f/-original-imah4khx9u3zxdbk.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/y/x/f/-original-imah4khx9u3zxdbk.jpeg?q=70',
+        description: 'Experience lightning-fast performance with the realme P1 5G, featuring 128GB storage and 6GB RAM. Perfect for multitasking and gaming.'
     },
     {
         id: 12,
         name: 'vivo V40 5G (Lotus Purple, 256 GB)  (8 GB RAM)',
         price: 36999,
         category: 'Electronics',
-        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/f/m/m/-original-imah3hvk9kgfsads.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/f/m/m/-original-imah3hvk9kgfsads.jpeg?q=70',
+        description: 'Unleash superior performance with the vivo V40 5G 5G. Featuring 256GB storage, 12GB RAM, and a stunning Mystic Black finish, it’s built for power users.'
     }, 
     {
         id: 13,
         name: 'vivo T3x 5G (Sapphire Blue, 128 GB)  (6 GB RAM)',
         price: 13999,
         category: 'Electronics',
-        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/d/x/1/-original-imah4kxes2ktzf9f.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/d/x/1/-original-imah4kxes2ktzf9f.jpeg?q=70',
+        description: 'Immerse yourself in sound with Sony WH-1000XM5 headphones, offering premium noise cancellation, high-resolution audio, and wireless convenience.'
     },
     {
         id: 14,
         name: 'vivo V40e 5G (Royal Bronze, 256 GB)  (8 GB RAM)',
         price: 28999,
         category: 'Electronics',
-        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/7/v/f/v40e-5g-v2403-vivo-original-imah6yshxyvnyfnt.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/7/v/f/v40e-5g-v2403-vivo-original-imah6yshxyvnyfnt.jpeg?q=70',
+        description: 'Lorem ipsum jjdndn sjsjksk fkfkmfm gwvbenn zkkxkkx hhgiejrnen dbbdggwha'
     }, 
     {
         id: 15,
         name: 'vivo T3 5G (Cosmic Blue, 128 GB)  (8 GB RAM)',
         price: 18499,
         category: 'Electronics',
-        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/g/d/1/-original-imagz6tzjgggzygu.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/g/d/1/-original-imagz6tzjgggzygu.jpeg?q=70',
+        description: 'Lorem ipsum jjdndn sjsjksk fkfkmfm gwvbenn zkkxkkx hhgiejrnen dbbdggwha'
     },
     {
         id: 16,
         name: 'HP AMD Ryzen 5 Hexa Core 5600H - (8 GB/512 GB SSD/Windows 11 Home/AMD Radeon AMD) 15-fb0153AX Gaming Laptop  (15.6 Inch, Performance Blue, Chrome Logo, 2.29 kg, With MS Office)',
         price: 50000,
         category: 'Electronics',
-        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/computer/x/0/g/15-fb0150ax-gaming-laptop-hp-original-imagrwmg9nw5ayzf.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/computer/x/0/g/15-fb0150ax-gaming-laptop-hp-original-imagrwmg9nw5ayzf.jpeg?q=70',
+        description: 'Lorem ipsum jjdndn sjsjksk fkfkmfm gwvbenn zkkxkkx hhgiejrnen dbbdggwha'
     },
     {
         id: 17,
         name: 'HP Victus AMD Ryzen 5 Hexa Core 5600H - (16 GB/512 GB SSD/Windows 11 Home/4 GB Graphics/AMD Radeon RX 6500M) 15-fb0134AX Gaming Laptop  (15.6 Inch, Performance Blue, 2.37 Kg)',
         price: 52000,
         category: 'Electronics',
-        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/computer/8/i/j/-original-imagthcbgurdzwuc.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/computer/8/i/j/-original-imagthcbgurdzwuc.jpeg?q=70',
+        description: 'Lorem ipsum jjdndn sjsjksk fkfkmfm gwvbenn zkkxkkx hhgiejrnen dbbdggwha'
     },
     {
         id: 18,
         name: 'Ultimus Elite Intel Core i5 10th Gen 1035G4 - (8 GB/512 GB SSD/Windows 11 Home) NU14U3INF56BN-MB Thin and Light Laptop  (14.1 Inch, Matt Black, 1.3 Kg)',
         price: 25000,
         category: 'Electronics',
-        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/computer/g/c/y/nu14u3inf56bn-mb-thin-and-light-laptop-ultimus-original-imagrtanznjrgb3u.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/312/312/xif0q/computer/g/c/y/nu14u3inf56bn-mb-thin-and-light-laptop-ultimus-original-imagrtanznjrgb3u.jpeg?q=70',
+        description: 'Lorem ipsum jjdndn sjsjksk fkfkmfm gwvbenn zkkxkkx hhgiejrnen dbbdggwha'
     },
     {
         id: 19,
         name: 'Athleisure Men Mesh White&Grey Walking Shoes Running Shoes For Men  (White , 8)',
         price: 1343,
         category: 'Footwear',
-        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/l/j/k/10-hrxecsp664-hrx-by-hrithik-roshan-white-grey-original-imah4k4pzzzjj79g.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/l/j/k/10-hrxecsp664-hrx-by-hrithik-roshan-white-grey-original-imah4k4pzzzjj79g.jpeg?q=70',
+        description: 'Lorem ipsum jjdndn sjsjksk fkfkmfm gwvbenn zkkxkkx hhgiejrnen dbbdggwha'
     },
     {
         id: 20,
         name: 'OGIY RETRO SHOES HIGH PREMIUM QUALITY Sneakers For Men  (White , 6)',
         price: 599,
         category: 'Footwear',
-        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/3/h/r/6-oogiy-40-aeonik-white-original-imagxggbkjyu2gze.jpeg?q=70'
+        image: 'https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/3/h/r/6-oogiy-40-aeonik-white-original-imagxggbkjyu2gze.jpeg?q=70',
+        description: 'Lorem ipsum jjdndn sjsjksk fkfkmfm gwvbenn zkkxkkx hhgiejrnen dbbdggwha'
     }, 
     {
         id: 21,
